2736ee8ab157a5ee975265562d088e415cb14460
Marrouche <ramboxxrhino2004@gmail.com>
2023-05-02 20:05:52 +0200

    [AJOUT] > modifications de php et bd

--- depot/Equipe2/html/src/login.php
+++ depot/Equipe2/html/src/login.php
@@ -1,14 +1,50 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<meta charset="UTF-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<link rel="stylesheet" href="../../dist/output.css">
-		<title>Connexion</title>
-	</head>
 	<body>
-		<h1>Formulaire de connexion</h1>
+		<?php
+
+		$dbuser = 'review_site';
+		$dbpass = 'password';
+		$conn = new mysqli("localhost", $dbuser, $dbpass, "momentos");
+		$conn->set_charset("utf8");
+
+		if (! $conn->connect_error) 
+			echo "Connecté";
+		else
+			echo "erreurrrrrrrrrrrrrrr";
+
 	
+		$userSaisi = $_POST["name"];
+		$mdpSaisi  = $_POST["password"];
+
+		echo $userSaisi;
+		echo $mdpSaisi;
+
+		$query = "SELECT * FROM USERS WHERE username = '" . $userSaisi . "' AND mdp = '" . $mdpSaisi . "';";
+
+		$queryArray = $conn->query($query)->fetch_array();
+
+		if ($queryArray == NULL) 
+		{
+			$sql = "INSERT INTO USERS VALUES ( '" . $userSaisi . "' , '" . $mdpSaisi . "')";
+			if (! $conn->query($sql)) {
+				echo "Erreur : " . $conn->error;
+			}
+		}
+		
+		// Requete select tout from users
+		$sql = "SELECT * FROM USERS;";
+
+		$result = $conn->query($sql);
+		if ($result->num_rows > 0) 
+		{
+			while ($row = $result->fetch_assoc()) 
+			{
+				echo "username : " . $row["username"]. "password : " . $row["mdp"]. "<br>";
+			}
+		}
+
+	
+		?>
 	</body>
 </html>
